4467bb427b9c4eb4f296ef9f893c2172c8760bc0
Annie <a.janice.chen@gmail.com>
2022-06-30 21:50:08 +0200

    cotton candy style sheet made

---
+++ Styles/CottonCandy.js
@@ -0,0 +1,47 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+export default StyleSheet.create({
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
+   h1: {
+      color: '#9D8189',
+      fontSize: 32
+   },
+   h2: {
+      fontSize: 25,
+      textAlign: 'center',
+      color: '#9D8189'
+   },
+   picker:{
+      width:  300
+   },
+   map: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+   },
+   item: {
+      backgroundColor: '#D8E2DC',
+      alignItems: 'center',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+   },
+   name:{
+      color:  '#9D8189',
+      fontWeight: 'bold'
+   },
+   input: {
+      height: 40,
+      width: 300,
+      margin: 12,
+      borderWidth: 1,
+      borderColor: '#9D8189',
+      backgroundColor: '#D8E2DC',
+      borderRadius: 10,
+      padding: 10,
+   },
+});
