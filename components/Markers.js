4e3df00b66d5fda992f908f1aab02290d8d1f709
Annie <a.janice.chen@gmail.com>
2022-07-01 13:49:36 +0200

    yeahw e dotn do this anymore

--- components/Markers.js
+++ components/Markers.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
 
 //loads home
 export default function Markers() {
@@ -13,8 +14,10 @@ export default function Markers() {
       headers: myHeadersGET
     }
 
+    //api url
     const apiUrl = 'https://stud.hosted.hr.nl/0999525/boba_marker_locations.json'
 
+    //fetch api
     const loadJSON = () => {
       fetch(apiUrl, myInitGET)
       .then(res => res.json())
@@ -22,20 +25,22 @@ export default function Markers() {
       .catch(error => console.log(error))
     }
 
+    //put markers in an array named markers that we makde earlier
     function updateData(data){
       setMarkers(data)
     }
 
+    //useEffect to initialize fetch
     useEffect(() => {loadJSON()}, [])
 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
